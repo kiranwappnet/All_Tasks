1d22c5d548551e50acc18cd2c06e4e52e36b84fa
kiranwappnet <kiranrwappnet@gmail.com>
2022-12-01 11:37:13 +0530

    All DONE

--- Method_Overriding/example.php
+++ Method_Overriding/example.php
@@ -1,21 +1,54 @@
 <?php
-class Student{
-    function show(){
-        echo "Student shows their all records";
+    // parent class
+    class Software_Developer {
+        // public property name
+        private $name;
+        
+        // public method
+
+        public function setname($name){
+            $this->name = $name;
+        }
+        public function salary() {
+            echo "Hi My Name Is " .$this->name. " My salary is 45,000 Per month..<br/>";
+        }
     }
-}
-class Employee extends Student{
-    function show(){
-        echo "Employee shows their all records";
+    
+    // child class
+    class Mobile_Developer extends Software_Developer {
+        
+        public function salary() {
+            echo "Hi My Name Is " .$this->name.  " My salary is 50,000 Per month..<br/>";
+        }
+        
     }
+    
+    // child class
+    class Node_js extends Software_Developer {
+        
+        public function salary() {
+            echo "Hi My Name Is " .$this->name. " My salary is 55,000 Per month..<br/>";;
+        }
+        
+    }
+    
+    $new = new Software_Developer();
+    $new->setname("KIRAN");
+    
+    
+    // calling child class method
+    
+    $new->salary();
+    
+    $S_new = new Mobile_Developer();
+    $S_new->name = "Riddhi Mahera";
+    
+    // calling child class method
+    $S_new->salary();
 
-}
-$ram = new Student;
-$ram->show();
+    $T_new = new Node_js();
+    $T_new->name = "Sonali Joshi";
 
-echo "</br>";
-
-$teacher = new Employee;
-$teacher->show();
-
+    $T_new->salary();
+    
 ?>
